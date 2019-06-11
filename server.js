57d370fc8d78ce6150b178812eb3f25d2d142cee
Dejan Belic <dejan@Dejans-MacBook-Pro.local>
2019-06-11 19:34:20 +0200

    Connected with mongodb.

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 console.log('Server side code running.');
 const express = require('express');
-
+const MongoClient = require('mongodb').MongoClient;
+const url = require('./config/key').mongoURI;
 const app = express();
 
 // Serve files from public directory.
 app.use(express.static('public'));
 
-app.listen(8080, () => {
-  console.log('listening on 8080')
+// Connect to the db and start express server.
+let db;
+
+MongoClient.connect(url,(err, database) => {
+  if (err) {
+    return console.log(err)
+  }
+  db = database;
+  app.listen(8080, () => {
+    console.log('listening on 8080')
+  });
 });
+
 
 // Serve the home page.
 app.get('/', (req, res) => {
